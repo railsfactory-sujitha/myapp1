a4a4e478107252f750aa40722a6d3b62ab8bef6b
Sujitha <sujitha@railsfactory.org>
2014-08-20 09:57:20 +0530

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-root :to => 'welcome#index'
-resources :welcome
+root :to => 'welcomes#index'
+resources :welcomes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
